38b5f26b1b17c869f41bae24b1cdaee508bf2cce
bart <bart_voet@telenet.be>
2019-10-06 16:59:48 +0200

    [build] improve c-code-extraction-script

--- extract_c_code_from_markdown.py
+++ extract_c_code_from_markdown.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import re
+import getopt
+import fnmatch
 
 def write_to_file(file_object,code):
     file_object.write(code)
@@ -38,15 +40,46 @@ def extract_if_applicable(code):
          else:
              append_to_file(filename,code)
 
-file_buffer = ""
-buffer_read = False
+def process_file(f):
+    file_buffer = ""
+    buffer_read = False
 
-for line in sys.stdin:
-    if line.startswith("```c") or line.startswith("~~~c"):
-        buffer_read = True
-    elif line.startswith("```")  or line.startswith("~~~"):
-        extract_if_applicable(file_buffer)
-        buffer_read = False
-        file_buffer = ""
-    elif buffer_read:
-        file_buffer = file_buffer + line
+    for line in f:
+        if line.startswith("```c") or line.startswith("~~~c"):
+            buffer_read = True
+        elif line.startswith("```")  or line.startswith("~~~"):
+            extract_if_applicable(file_buffer)
+            buffer_read = False
+            file_buffer = ""
+        elif buffer_read:
+            file_buffer = file_buffer + line
+
+def process_all_files_in_directory(directory):
+  matches = []
+  for root, dirnames, filenames in os.walk(directory):
+      for filename in fnmatch.filter(filenames, '*.md'):
+          md_file = open(os.path.join(root, filename), "r")
+          process_file(md_file)
+          md_file.close
+
+def main(argv):
+   directory = None
+   output_directory = ''
+   try:
+      opts, args = getopt.getopt(argv,"hf:d:o:",["help","file","dir=","output="])
+   except getopt.GetoptError:
+      print 'test.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt in ("-d", "--dir"):
+         directory = arg
+   
+   if directory is None:
+      process_file(sys.stdin)
+   else:
+      process_all_files_in_directory(directory)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+# -d for directory -f for file -o for output-directory (otherwise it's the 
